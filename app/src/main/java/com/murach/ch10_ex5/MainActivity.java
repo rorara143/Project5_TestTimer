1ef6fa61cbfd81de2f665c381da3733e0e428fe7
rorara143 <ramonavitin@gmail.com>
2016-11-23 13:16:57 -0800

    secondtimes

--- app/src/main/java/com/murach/ch10_ex5/MainActivity.java
+++ app/src/main/java/com/murach/ch10_ex5/MainActivity.java
@@ -1,30 +1,47 @@
 package com.murach.ch10_ex5;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.widget.TextView;
+public class MainActivity extends Activity implements OnClickListener{
 
-public class MainActivity extends Activity {
+    private TextView messageTextView;
+    private Button startbutton;
+    private Button stopbutton;
+    private Timer restart;
 
-    private TextView messageTextView; 
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        //reference into widget
         messageTextView = (TextView) findViewById(R.id.messageTextView);
+        startbutton = (Button) findViewById(R.id.startbutton);
+        stopbutton = (Button) findViewById(R.id.stopbutton);
+
+
+        //set listener
+        startbutton.setOnClickListener(this);
+        stopbutton.setOnClickListener(this);
         startTimer();
     }
-    
+
+
+
+
     private void startTimer() {
         final long startMillis = System.currentTimeMillis();
         Timer timer = new Timer(true);
         TimerTask task = new TimerTask() {
-            
+
             @Override
             public void run() {
                 long elapsedMillis = System.currentTimeMillis() - startMillis;
@@ -42,8 +59,30 @@ public class MainActivity extends Activity {
 
             @Override
             public void run() {
-                messageTextView.setText("Seconds: " + elapsedSeconds);
+                messageTextView.setText("Seconds: " + elapsedSeconds / 10 + " time(s). ");
             }
         });
-    }    
+    }
+
+    @Override
+    //
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+
+
+    @Override
+    public void onClick(View view) {
+switch (view.getId()) {
+    case R.id.startbutton:
+        startTimer();
+        break;
+    case R.id.stopbutton:
+        restart.cancel();
+        break;
 }
+    }
+}
+
