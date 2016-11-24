3e46d4eed83f283f3cdbf5017d74c9f05cb26e7b
rorara143 <ramonavitin@gmail.com>
2016-11-23 16:25:42 -0800

    done

--- app/src/main/java/com/murach/ch10_ex5/MainActivity.java
+++ app/src/main/java/com/murach/ch10_ex5/MainActivity.java
@@ -1,12 +1,18 @@
 package com.murach.ch10_ex5;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -17,10 +23,14 @@ public class MainActivity extends Activity implements OnClickListener{
     private Button stopbutton;
     private Timer restart;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         //reference into widget
         messageTextView = (TextView) findViewById(R.id.messageTextView);
@@ -31,7 +41,7 @@ public class MainActivity extends Activity implements OnClickListener{
         //set listener
         startbutton.setOnClickListener(this);
         stopbutton.setOnClickListener(this);
-        startTimer();
+
     }
 
 
@@ -45,6 +55,29 @@ public class MainActivity extends Activity implements OnClickListener{
             @Override
             public void run() {
                 long elapsedMillis = System.currentTimeMillis() - startMillis;
+                final String FILENAME = "news_feed.xml";
+                try {
+                    //get the input in the stream
+                    URL url = new URL("http://rss.cnn.com/rss/cnn_tech.rss");
+                    InputStream in = url.openStream();
+
+                    //get the output stream
+                    FileOutputStream out = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+
+                    //read input and write output
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = in.read(buffer);
+                    while (bytesRead != -1) {
+                        out.write(buffer, 0, bytesRead);
+                        bytesRead = in.read(buffer);
+                    }
+                    out.close();
+                    in.close();
+                }
+                catch (IOException e) {
+                    Log.e("News reader", e.toString());
+                }
+
                 updateView(elapsedMillis);
             }
         };
@@ -59,13 +92,13 @@ public class MainActivity extends Activity implements OnClickListener{
 
             @Override
             public void run() {
-                messageTextView.setText("Seconds: " + elapsedSeconds / 10 + " time(s). ");
+                messageTextView.setText("Seconds: " + elapsedSeconds / 10);
             }
         });
     }
 
     @Override
-    //
+    //cancels the timer when the app is paused.
     protected void onPause() {
         super.onPause();
     }
